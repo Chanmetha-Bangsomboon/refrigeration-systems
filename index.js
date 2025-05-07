f7bedfdc94cbf67f7279cb56daf7d2f66f52ba05
Chanmetha-Bangsomboon <chanmetha.17520@gmail.com>
2025-05-08 00:03:56 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('config index.html');
+function showSidebar1(){
+   const sidebar1 = document.querySelector('.sidebar1')
+   sidebar1.style.width = '250px'
+}
+function closSidebar1(){
+    const sidebar = document.querySelector('.sidebar1')
+    sidebar.style.width = '0px'    
+}
